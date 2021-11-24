0e063c79ccf919253365604d8bcc374936262a20
youthke <47767005+youthke@users.noreply.github.com>
2021-11-25 01:04:38 +0900

    add css stack detail

--- components/stacks/Detail.tsx
+++ components/stacks/Detail.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Container, Row } from "reactstrap";
 import { useStackState } from "../../ducks/stack/selectors";
 import Questions from "../questions/List";
 
@@ -7,15 +8,23 @@ const StackDetail: React.FC = () => {
   const state = useStackState().stack
   if(!state.stack) return <></>
   return (
-    <>
-      <h1>{state.stack.name}</h1>
-      <h3>{state.stack.description}</h3>
+    <Container className="justify-content-center">
+      <Row  className="justify-content-center">
+        <Col md="auto">
+          <h1>{state.stack.name}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>{state.stack.description}</h3>
+        </Col>
+      </Row>
       {
         state.stack.questions.length == 0 ?
         <h3>質問はありません</h3>:
         <Questions questions={state.stack.questions}/>
       }
-    </>
+    </Container>
   );
 }
 
